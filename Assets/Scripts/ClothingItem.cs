ef01e98cd2d677243831c784faa48ff493bf4a9e
Boyu Cao <boyu.cao@student.saedubai.com>
2025-11-09 17:24:01 +0400

    Sorting clothe mini game
    It's very buggy, clothes are not suppose to be destroyed once placed in the correct slot, but it's being destroyed for some reason, and whenever it's sorted, the game pauses, this needs to be fixed

---
+++ Assets/Scripts/ClothingItem.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public enum ClothingType { Shirt, Pants, Underwear }
+
+public class ClothingItem : MonoBehaviour
+{
+    public ClothingType type;
+}
---
+++ Assets/Scripts/SortingZone.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class SortingZone : MonoBehaviour
+{
+    public ClothingType acceptedType;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var clothing = other.GetComponent<ClothingItem>();
+        if (clothing == null) return;
+
+        if (clothing.type == acceptedType)
+        {
+            Debug.Log("Correctly sorted: " + clothing.type);
+            Destroy(clothing.gameObject); 
+        }
+        else
+        {
+            Debug.Log("Wrong slot!");
+        }
+    }
+}
+
