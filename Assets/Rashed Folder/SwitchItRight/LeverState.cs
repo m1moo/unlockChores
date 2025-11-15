c36ab73db5a77c608122bccfe04a69121b912895
RashedRizwan <rashed.manegar@student.saedubai.com>
2025-11-15 17:09:58 +0400

    Switch It Right
    Done with my second game, also made few changes with the folders made one common foloder (rashed) and put all my minigames there

---
+++ Assets/Rashed Folder/SwitchItRight/LeverState.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class LeverState : MonoBehaviour
+{
+    public bool isUp;
+    public LeverPuzzleManager manager;
+
+    void Update()
+    {
+        // Get lever rotation
+        float z = transform.localEulerAngles.z;
+
+        // Convert angle from 0–360 to -180–180
+        if (z > 180) z -= 360;
+
+        // Lever is UP if angle is above 0
+        bool newState = z > 0;
+
+        // Only update if it changed (prevents spam)
+        if (newState != isUp)
+        {
+            isUp = newState;
+            manager.CheckPuzzle();     // Check puzzle every time a lever is moved
+        }
+    }
+}
