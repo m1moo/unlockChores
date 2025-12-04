d7b1a86ca604a02c864abdafdd4e4598f715937f
Charlie <boyu.cao@student.saedubai.com>
2025-12-05 02:36:01 +0400

    updated Clothe sorting mini game
    replaced models, added sound effects, properly scaled, added particle effects

--- Assets/Boyu Cao/SortingZone.cs
+++ Assets/Boyu Cao/SortingZone.cs
@@ -4,20 +4,60 @@ public class SortingZone : MonoBehaviour
 {
     public ClothingType acceptedType;
 
+    [Header("Sound Effects")]
+    public AudioSource audioSource;
+    public AudioClip correctSound;
+    public AudioClip wrongSound;
+
+    [Header("Particle Effects")]
+    public ParticleSystem correctEffectPrefab;
+    public ParticleSystem wrongEffectPrefab;
+
     private void OnTriggerEnter(Collider other)
     {
         var clothing = other.GetComponent<ClothingItem>();
         if (clothing == null) return;
 
-        if (clothing.type == acceptedType)
+        bool correct = clothing.type == acceptedType;
+
+        // Play sound
+        PlaySound(correct);
+
+        // Play particles
+        PlayParticles(correct, clothing.transform.position);
+
+        if (correct)
         {
             Debug.Log("Correctly sorted: " + clothing.type);
-            Destroy(clothing.gameObject); 
+            Destroy(clothing.gameObject);
         }
         else
         {
             Debug.Log("Wrong slot!");
         }
     }
+
+    private void PlaySound(bool correct)
+    {
+        if (audioSource == null) return;
+
+        if (correct && correctSound != null)
+            audioSource.PlayOneShot(correctSound);
+        else if (!correct && wrongSound != null)
+            audioSource.PlayOneShot(wrongSound);
+    }
+
+    private void PlayParticles(bool correct, Vector3 position)
+    {
+        ParticleSystem prefab = correct ? correctEffectPrefab : wrongEffectPrefab;
+        if (prefab == null) return;
+
+        ParticleSystem effect = Instantiate(prefab, position, Quaternion.identity);
+        effect.Play();
+
+        Destroy(effect.gameObject, effect.main.duration + 0.1f);
+    }
 }
+
+
 
